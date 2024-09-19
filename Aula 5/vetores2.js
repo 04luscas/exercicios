c0f7fd1d03be1e9d61fd578f7e4d135f61b75db7
Lucas <lucasalmeidapereira09@gmail.com>
2024-09-19 16:55:02 -0300

    Add files via upload

---
+++ Aula 5/vetores2.js
@@ -0,0 +1,59 @@
+//exercicio 1
+// let vetor = [20, 30, 40];
+// let vezes = 2;
+
+// let resultado = vetor.map(num =>num *= vezes);
+// console.log(resultado)
+
+// exercicio 2
+// let vetor = [20, 30, 52, 43, 57, 68, 27, 13];
+// vetor.sort ();
+// console.log(vetor)
+
+// exercicio 3
+// let vetor = [15, 43, 9, 22, 58, 81, 32];
+// let maiorNumero = Math.max(...vetor);
+// console.log(`O maior número no vetor é: ${maiorNumero}`);
+
+// exercicio 4 
+// let numeros = [1, 2, 3, 4,];
+// let par = numeros.some(num => num % 2 === 0);
+// console.log(numeros);
+// console.log(par);
+
+// exercicio 6
+//  let vetor = [2, 5, 7, 11, 25, 27,];
+//  let maiorNumero = Math.max(...vetor);
+//  console.log(`O maior número no vetor é: ${maiorNumero}`);
+
+// exercicio 11
+// let matriz = [
+// [1, 2, 3],
+// [4, 5, 6],
+// [7, 8, 9]
+// ];
+// let soma = 0
+
+// for (let i = 0; i < matriz.length; i++) {
+//     for (let j = 0; j < matriz[i].length; j++) {
+//       soma += matriz[i][j]
+//     }
+//   }
+//   console.log(soma)
+
+// exercicio 12
+// let matriz = [
+// [1, 2, 3],
+// [4, 5, 6],
+// [7, 8, 9]
+// ];
+// let maior = 0
+
+// for (let i = 0; i < matriz.length; i++) {
+//     for (let j = 0; j < matriz[i].length; j++) 
+//         if (matriz[i][j] > maior){
+//        maior = matriz[i][j]
+//         }
+// }
+// console.log(maior)
+// console.log(`O maior número no vetor é: ${maior}`);
